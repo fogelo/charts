5597ebf3d81c4e8eb66b1d917d0158a41f4dde62
fogelo <fogelo@yandex.ru>
2022-08-04 10:19:03 +0300

    face part and change readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Useful refs
 
-Много готовых примеров графиков на d3 - [https://observablehq.com/](https://observablehq.com/)
+Много готовых примеров графиков на d3 - [https://observablehq.com/](https://observablehq.com/)/
+d3-shape, про то как создавать геометрические фигуры на d3
+- [https://github.com/d3/d3-shape](https://github.com/d3/d3-shape)
--- src/charts/FreeCodeCamp/FreeCodeCamp.tsx
+++ src/charts/FreeCodeCamp/FreeCodeCamp.tsx
@@ -1,10 +1,52 @@
 import React from 'react';
+import "./FreeCodeCamp.scss"
+import * as d3 from "d3"
+import {PieArcDatum} from "d3";
+
 
 const FreeCodeCamp = () => {
-    return (
-        <div>
+    const width = 960
+    const height = 500
+    const centerX = width / 2
+    const centerY = height / 2
+    const strokeWidth = 20
+    const eyeOffsetX = 90
+    const eyeOffsetY = 100
+    const eyeRadius = 40
+    const mouthWidth = 20
+    const mouthRadius = 140
 
-        </div>
+
+    const mouthArc = d3.arc()
+        .innerRadius(mouthRadius)
+        .outerRadius(mouthRadius+mouthWidth)
+        .startAngle(Math.PI / 2)
+        .endAngle(3 / 2 * Math.PI);
+
+
+    return (
+        <svg className={"free-code-camp-svg"} width={width} height={height}>
+            <g transform={`translate(${centerX}, ${centerY})`}>
+                <circle
+                    r={centerY - strokeWidth / 2}
+                    fill={"grey"}
+                    stroke={"black"}
+                    strokeWidth={strokeWidth}
+                />
+                {/*<circle*/}
+                {/*    cx={-eyeOffsetX}*/}
+                {/*    cy={-eyeOffsetY}*/}
+                {/*    r={eyeRadius}*/}
+                {/*/>*/}
+                {/*<circle*/}
+                {/*    cx={+eyeOffsetX}*/}
+                {/*    cy={-eyeOffsetY}*/}
+                {/*    r={eyeRadius}*/}
+                {/*/>*/}
+                {/*@ts-ignore*/}
+                <path d={mouthArc()}/>
+            </g>
+        </svg>
     );
 };
 
