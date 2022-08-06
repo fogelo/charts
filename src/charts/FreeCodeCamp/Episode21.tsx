745e26c79329c4737921ba30ea4c59c9da611baa
antonorlov <fogelo@yandex.ru>
2022-08-06 15:44:48 +0300

    episode 21

--- src/charts/FreeCodeCamp/Episode21.tsx
+++ src/charts/FreeCodeCamp/Episode21.tsx
@@ -1,24 +1,55 @@
-import React, {useEffect, useState} from 'react';
-import {DSVRowArray} from "d3";
+import React, {useEffect, useState} from "react";
 import * as d3 from "d3";
 
 const Episode21 = () => {
 
+    const width = 960
+    const height = 400
+
+
     const csvUrl = "https://gist.githubusercontent.com/fogelo/6c7f8a44911a1640b52579d6998f82d9/raw/a6cc67d27005e324e2c7956aca6764b444dd692a/population"
 
-    const [data, setData] = useState<DSVRowArray<string> | null>(null)
+    const [data, setData] = useState<any>(null)
 
     useEffect(() => {
-        d3.csv(csvUrl).then((res) => {
+        const row = (d: any) => {
+            d.Population = +d["2020"]
+            return d
+        }
+        // @ts-ignore
+        d3.csv(csvUrl, row).then((res) => {
             setData(res)
         })
 
     }, [])
+
+    if (!data) {
+        return <div>Loading</div>
+    }
+
+    const countries = data.map((d: any) => d.Country).slice(0, 10)
+    const yScale = d3.scaleBand().domain(countries).range([0, height])
+    // @ts-ignore
+    const xScale = d3.scaleLinear().domain([0, d3.max(data, d => d.Population)]).range([0, width])
     console.log(data)
+    console.log(countries)
+
+
+    // @ts-ignore
+
+
     return (
-        <div>
-            
-        </div>
+        <svg width={width} height={height}>
+            {
+                data.map((d: any, i: any) => {
+                    return (
+                        <rect key={i} x={0} y={yScale(d.Country)} width={xScale(d.Population)}
+                              height={yScale.bandwidth()}/>
+                    )
+                })
+            }
+            {/*<rect width={100} height={100}/>*/}
+        </svg>
     );
 };
 
