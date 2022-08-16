53b331195738b8a857991746a5094c27e9c97159
fogelo <fogelo@yandex.ru>
2022-08-16 16:25:42 +0300

    episode 25 - Making a Scatter Plot

--- src/charts/FreeCodeCamp/FreeCodeCamp.tsx
+++ src/charts/FreeCodeCamp/FreeCodeCamp.tsx
@@ -4,15 +4,17 @@ import Face from "./Face";
 import MouseMove from "./MouseMove";
 import Episode20 from "./Episode20";
 import Episode21 from "./Episode21";
+import Episode25 from "./Episode25";
 
 const FreeCodeCamp = () => {
     return (
-       <div>
-           {/*<Face/>*/}
-           {/*<MouseMove/>*/}
-           {/*<Episode20/>*/}
-           <Episode21/>
-       </div>
+        <div>
+            {/*<Face/>*/}
+            {/*<MouseMove/>*/}
+            {/*<Episode20/>*/}
+            {/*<Episode21/>*/}
+            <Episode25/>
+        </div>
     );
 };
 
